eb579e73c8cef296ec81ab48361d7f82bcb77108
moscoltd <moscooinnovatorsltd@gmail.com>
2023-12-10 17:46:59 +0300

    ssss

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Mobile_Shopee-E-Commerce-Website
-In this complete E-commerce Website, We will take a look at how to create Mobile Shopee Using PHP and Mysql Database. We gonna start by creating an HTML template then convert it into PHP and then use MySQL database to fetch products and display it in the project.
+# theBag online shop website
+is a commercial website dealing with solor and renewable energy
+is developed by mosco
+cureently is still in progress
--- database/shopee.sql
+++ database/shopee.sql
@@ -54,19 +54,19 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`item_id`, `item_brand`, `item_name`, `item_price`, `item_image`, `item_register`) VALUES
-(1, 'Samsung', 'Samsung Galaxy 10', 152.00, './assets/products/1.png', '2020-03-28 11:08:57'), -- NOW()
-(2, 'Redmi', 'Redmi Note 7', 122.00, './assets/products/2.png', '2020-03-28 11:08:57'),
-(3, 'Redmi', 'Redmi Note 6', 122.00, './assets/products/3.png', '2020-03-28 11:08:57'),
-(4, 'Redmi', 'Redmi Note 5', 122.00, './assets/products/4.png', '2020-03-28 11:08:57'),
-(5, 'Redmi', 'Redmi Note 4', 122.00, './assets/products/5.png', '2020-03-28 11:08:57'),
-(6, 'Redmi', 'Redmi Note 8', 122.00, './assets/products/6.png', '2020-03-28 11:08:57'),
-(7, 'Redmi', 'Redmi Note 9', 122.00, './assets/products/8.png', '2020-03-28 11:08:57'),
-(8, 'Redmi', 'Redmi Note', 122.00, './assets/products/10.png', '2020-03-28 11:08:57'),
-(9, 'Samsung', 'Samsung Galaxy S6', 152.00, './assets/products/11.png', '2020-03-28 11:08:57'),
-(10, 'Samsung', 'Samsung Galaxy S7', 152.00, './assets/products/12.png', '2020-03-28 11:08:57'),
-(11, 'Apple', 'Apple iPhone 5', 152.00, './assets/products/13.png', '2020-03-28 11:08:57'),
-(12, 'Apple', 'Apple iPhone 6', 152.00, './assets/products/14.png', '2020-03-28 11:08:57'),
-(13, 'Apple', 'Apple iPhone 7', 152.00, './assets/products/15.png', '2020-03-28 11:08:57');
+(1, 'Solar', '24 watts monocrystalline solar', 152.00, './assets/products/1.png', '2020-03-28 11:08:57'), -- NOW()
+(2, 'Solar', '24 volts Polycrystalline solar', 122.00, './assets/products/2.png', '2020-03-28 11:08:57'),
+(3, 'Solar', '100 watts solar(dark)', 122.00, './assets/products/3.png', '2020-03-28 11:08:57'),
+(4, 'Solar', '2200 watts solar', 122.00, './assets/products/4.png', '2020-03-28 11:08:57'),
+(5, 'Battery', '24 volts solar battery', 122.00, './assets/products/5.png', '2020-03-28 11:08:57'),
+(6, 'Battery', '200 watts 12 volts solar battery', 122.00, './assets/products/6.png', '2020-03-28 11:08:57'),
+(7, 'Battery', '800 watts battery 24 volts', 122.00, './assets/products/8.png', '2020-03-28 11:08:57'),
+(8, 'Battery', 'Green solar battery', 122.00, './assets/products/10.png', '2020-03-28 11:08:57'),
+(9, 'Solar', 'thin film portable solar', 152.00, './assets/products/11.png', '2020-03-28 11:08:57'),
+(10, 'Lights', '500 watts solar led', 152.00, './assets/products/12.png', '2020-03-28 11:08:57'),
+(11, 'Lights', '1200 wats outdoor solar lights', 152.00, './assets/products/13.png', '2020-03-28 11:08:57'),
+(12, 'Lights', 'stylish solar bulbs (silver)', 152.00, './assets/products/14.png', '2020-03-28 11:08:57'),
+(13, 'Lights', 'Outdoor solar lights', 152.00, './assets/products/15.png', '2020-03-28 11:08:57');
 
 -- --------------------------------------------------------
 
