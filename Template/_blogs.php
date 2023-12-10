8f0cf910bd3b143e8647063b982d8ae0c1ceaa28
moscoltd <moscooinnovatorsltd@gmail.com>
2023-12-10 17:33:21 +0300

    thebag

---
+++ Template/_blogs.php
@@ -0,0 +1,36 @@
+
+<!-- Blogs -->
+<section id="blogs">
+    <div class="container py-4">
+        <h4 class="font-rubik font-size-20">Technology Blogs</h4>
+        <hr>
+
+        <div class="owl-carousel owl-theme">
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">upcoming technology</h5>
+                    <img src="./assets/blog/blog1.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">The solar power industry is on the brink of a significant transformation with the development of new technologies that promise to increase the efficiency and reduce the cost of solar energy. Here are some of the most exciting advancements in solar technology:</p>
+                    <a href="#" class="color-second text-left">Read more</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Green energy</h5>
+                    <img src="./assets/blog/blog2.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">Green energy refers to energy that is produced using methods and sources that do not harm the natural environment. It often comes from renewable resources such as sunlight, wind, or water. Here are some key points about green energy:</p>
+                    <a href="#" class="color-second text-left">Read more</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Portable solar</h5>
+                    <img src="./assets/blog/blog3.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">Portable solar technology is a rapidly evolving field that offers a range of solutions for generating electricity on the go. Here are some of the latest advancements and options available:</p>
+                    <a href="#" class="color-second text-left">Read more</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- !Blogs -->
